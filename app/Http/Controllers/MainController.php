94fb0cb9d1083e78746537dd8e84f4eef900d9da
Mario <maritojhefi@outlook.com>
2021-05-22 02:20:56 -0400

    mejoras vista cliente 1.7

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
     public function menu(){
         
         
